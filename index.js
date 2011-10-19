5b2d4a595499714095250f0628c2a8bd3f9ff2bc
Felix Geisendörfer <felix@debuggable.com>
2011-10-19 16:07:30 +0200

    Initial implementation

---
+++ index.js
@@ -0,0 +1,126 @@
+var redis        = require('redis');
+var url          = require('url');
+var EventEmitter = require('events').EventEmitter;
+var util         = require('util');
+
+util.inherits(RedisPool, EventEmitter);
+
+module.exports = new RedisPool();
+module.exports.RedisPool = RedisPool;
+
+function RedisPool() {
+  EventEmitter.call(this);
+
+  this.length       = 0;
+
+  this._pool        = {};
+  this._exclusive   = [];
+  this._defaultHost = 'localhost';
+  this._defaultPort = 6379;
+}
+
+RedisPool.prototype.parse = function(dsn) {
+  var parsed = url.parse(dsn);
+  if (parsed.protocol !== 'redis:') {
+    throw new Error('RedisPool.UnknownProtocol: ' + parsed.protocol);
+  }
+
+  return {
+    host       : parsed.hostname || this._defaultHost,
+    port       : parsed.port || this._defaultPort,
+    namespace  : (parsed.pathname)
+      ? parsed.pathname.substr(1)
+      : '',
+  };
+};
+
+RedisPool.prototype.stringify = function(parsed) {
+  return url.format({
+    protocol : 'redis:',
+    slashes  : true,
+    hostname : parsed.host,
+    port     : parsed.port,
+    pathname : '/' + parsed.namespace,
+  });
+};
+
+RedisPool.prototype.alloc = function(dsn, options) {
+  var parsed = this.parse(dsn);
+  this.emit('log', 'Alloc: ' + dsn + ' (' + JSON.stringify(options) + ')');
+
+  return (options && options.exclusive)
+    ? this._allocExclusive(parsed)
+    : this._allocInPool(parsed);
+};
+
+RedisPool.prototype._createClient = function(parsedDsn) {
+  return redis.createClient(parsedDsn.port, parsedDsn.host);
+};
+
+RedisPool.prototype._allocExclusive = function(parsedDsn) {
+  var client = this._createClient(parsedDsn)
+  this._exclusive.push(client);
+  this.length++;
+
+  return client;
+};
+
+RedisPool.prototype._allocInPool = function(parsedDsn) {
+  var key = [parsedDsn.host, parsedDsn.port].join(':');
+  var ref = this._pool[key];
+
+  if (ref) {
+    ref.count++;
+    return ref.client;
+  }
+
+  this.length++;
+  this._pool[key] = ref = {
+    count  : 1,
+    client : this._createClient(parsedDsn),
+  };
+
+  return ref.client;
+};
+
+RedisPool.prototype.free = function(client) {
+  if (this._freePoolClient(client)) return;
+  if (this._freeExclusiveClient(client)) return;
+
+
+  var err = new Error('FreeError: Cannot free unknown client.');
+  err.client = client;
+  throw err;
+};
+
+RedisPool.prototype._freePoolClient = function(client) {
+  var matchingRef;
+  for (var key in this._pool) {
+    var ref = this._pool[key];
+    if (ref.client === client) {
+      matchingRef = ref;
+      break;
+    }
+  }
+
+  if (!matchingRef) return false;
+
+  if (!--matchingRef.count) {
+    delete this._pool[key];
+    ref.client.quit();
+    this.length--;
+  }
+
+  return true;
+};
+
+RedisPool.prototype._freeExclusiveClient = function(client) {
+  var index = this._exclusive.indexOf(client);
+  if (index === -1) return false;
+
+  this._exclusive.slice(index, 1);
+  client.quit();
+  this.length--;
+
+  return true;
+};
---
+++ test.js
@@ -0,0 +1,132 @@
+var RedisPool = require('./index').RedisPool;
+var singleton = require('./index');
+var assert    = require('assert');
+var noop      = function() {};
+var net       = require('net');
+
+// That's right, I do this
+net.createConnection = function() {
+  return new net.Stream();
+};
+
+(function testParse() {
+  (function testAll() {
+    var parsed = singleton.parse('redis://example.org:30/foo');
+    assert.equal(parsed.host, 'example.org');
+    assert.equal(parsed.port, 30);
+    assert.equal(parsed.namespace, 'foo');
+  })();
+
+  (function testDefaultPort() {
+    var parsed = singleton.parse('redis://localhost/');
+    assert.equal(parsed.port, 6379);
+  })();
+
+  (function testDefaultHost() {
+    var parsed = singleton.parse('redis://');
+    assert.equal(parsed.host, 'localhost');
+  })();
+
+  (function testNoNamespace() {
+    var parsed = singleton.parse('redis://foo');
+    assert.equal(parsed.namespace, '');
+  })();
+
+  (function testUnknownProtocol() {
+    assert.throws(function() {
+      singleton.parse('http://foo/');
+    }, /RedisPool.UnknownProtocol:/);
+  })();
+})();
+
+(function testStringify() {
+  (function testAll() {
+    var dsn = singleton.stringify({host: 'example.org', port: 70, namespace: 'foo'});
+    assert.equal(dsn, 'redis://example.org:70/foo');
+  })();
+})();
+
+(function testAlloc() {
+  (function testReturnsRedis() {
+    var pool = new RedisPool();
+    var client = pool.alloc('redis://localhost/');
+    assert.equal(client.host, 'localhost');
+  })();
+
+  (function testAllocationPool() {
+    var pool = new RedisPool();
+    assert.equal(pool.length, 0);
+
+    var a = pool.alloc('redis://localhost/');
+    assert.equal(pool.length, 1);
+
+    var b = pool.alloc('redis://localhost/');
+    assert.equal(pool.length, 1);
+
+    assert.strictEqual(a, b);
+
+    var c = pool.alloc('redis://localhost:9003/');
+    assert.equal(pool.length, 2);
+
+    assert.notStrictEqual(a, c);
+  })();
+
+  (function testExclusive() {
+    var pool = new RedisPool();
+    assert.equal(pool.length, 0);
+
+    var a = pool.alloc('redis://localhost/', {exclusive: true});
+    assert.equal(pool.length, 1);
+
+    var b = pool.alloc('redis://localhost/', {exclusive: true});
+    assert.equal(pool.length, 2);
+
+    assert.notStrictEqual(a, b);
+  })();
+})();
+
+(function testFree() {
+  (function testFreeNonExistingClient() {
+    var pool = new RedisPool();
+
+    assert.throws(function() {
+      pool.free({});
+    }, /FreeError/);
+  })();
+
+  (function testFreePoolClient() {
+    var pool = new RedisPool();
+    var a    = pool.alloc('redis://localhost:9003/')
+    var b    = pool.alloc('redis://localhost:9003/')
+
+    var quits = 0;
+    a.quit = function() {
+      quits++;
+    };
+
+    pool.free(a);
+    assert.equal(pool.length, 1);
+    assert.equal(quits, 0);
+
+    pool.free(b);
+    assert.equal(pool.length, 0);
+    assert.equal(quits, 1);
+  })();
+
+  (function testFreeExclusiveClient() {
+    var pool = new RedisPool();
+    var a    = pool.alloc('redis://localhost:9003/', {exclusive: true})
+    var b    = pool.alloc('redis://localhost:9003/', {exclusive: true})
+
+    assert.equal(pool.length, 2);
+
+    var quits = 0;
+    a.quit = function() {
+      quits++;
+    };
+
+    pool.free(a);
+    assert.equal(pool.length, 1);
+    assert.equal(quits, 1);
+  })();
+})();
